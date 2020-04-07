9bb047a8d4fe3f1b99c3200d49128be9fc6ba270
Jorge Ramirez <joramirez96@gmail.com>
2020-04-07 22:17:26 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,48 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name, :songs
+  @@all = []
+
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+    end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    # binding.pry
+    artist  = @@all.find do |artist|
+                    artist.name == name
+                  end
+        if artist == nil
+          artist = Artist.new(name)
+        else
+          artist
+        end
+# (1..10).find_all { |i|  i % 3 == 0 }
+  # binding.pry
+  end
+
+  def songs
+
+    Song.all.select do |song|
+      song.artist == self
+      # song.name ==
+    end
+  end
+  def add_song(song)
+    # binding.pry
+    song.artist = self
+  end
+
+
+end
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,22 @@
+require 'pry'
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+  end
+
+  def import
+
+    files = Dir.glob(@path + "/*.mp3")
+
+
+    files.each do |filename|
+      
+      Song.new_by_filename(filename)
+    end
+  end
+end
